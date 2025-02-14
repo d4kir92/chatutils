8df49f5388b2385920a9e1ad8611b78ed6276213
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-02-14 12:57:20 +0100

    v0.3.12

--- settings.lua
+++ settings.lua
@@ -47,14 +47,14 @@ end
 
 function ChatUtils:InitSettings()
     CHUT = CHUT or {}
-    ChatUtils:SetVersion("ChatUtils", 133457, "0.3.11")
+    ChatUtils:SetVersion("ChatUtils", 133457, "0.3.12")
     cu_settings = ChatUtils:CreateFrame(
         {
             ["name"] = "ChatUtils",
             ["pTab"] = {"CENTER"},
             ["sw"] = 520,
             ["sh"] = 520,
-            ["title"] = format("ChatUtils |T133457:16:16:0:0|t v|cff3FC7EB%s", "0.3.11")
+            ["title"] = format("ChatUtils |T133457:16:16:0:0|t v|cff3FC7EB%s", "0.3.12")
         }
     )
 
