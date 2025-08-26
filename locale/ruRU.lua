02b8cdf982a6d0ad364857bc070467a83103053a
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-08-26 19:37:45 +0500

    Update ruRU.lua

--- locale/ruRU.lua
+++ locale/ruRU.lua
@@ -1,11 +1,11 @@
--- ruRU Russian - ZamestoTV
+-- ruRU Russian
 local _, ChatUtils = ...
-ChatUtils:AddTrans("ruRU", "LID_GENERAL", "Общий")
+ChatUtils:AddTrans("ruRU", "LID_GENERAL", "Общее")
 ChatUtils:AddTrans("ruRU", "LID_CHAT", "Чат")
-ChatUtils:AddTrans("ruRU", "LID_SHOWMINIMAPBUTTON", "Кнопка на миникарте")
-ChatUtils:AddTrans("ruRU", "LID_SHOWROLEICON", "Показать значок роли")
-ChatUtils:AddTrans("ruRU", "LID_SHOWCLASSICON", "Показать значок класса")
-ChatUtils:AddTrans("ruRU", "LID_SHOWRACEICON", "Показать значок расы")
-ChatUtils:AddTrans("ruRU", "LID_SHOWITEMICON", "Показать значок предмета")
-ChatUtils:AddTrans("ruRU", "LID_SHOWPLAYERLEVEL", "Показать уровень игрока")
-ChatUtils:AddTrans("ruRU", "LID_USESMALLCHANNELNAMES", "Используйте короткие названия каналов")
+ChatUtils:AddTrans("ruRU", "LID_SHOWMINIMAPBUTTON", "Показывать кнопку на миникарте")
+ChatUtils:AddTrans("ruRU", "LID_SHOWROLEICON", "Показывать значок роли")
+ChatUtils:AddTrans("ruRU", "LID_SHOWCLASSICON", "Показывать значок класса")
+ChatUtils:AddTrans("ruRU", "LID_SHOWRACEICON", "Показывать значок расы")
+ChatUtils:AddTrans("ruRU", "LID_SHOWITEMICON", "Показывать значок предмета")
+ChatUtils:AddTrans("ruRU", "LID_SHOWPLAYERLEVEL", "Показывать уровень игрока")
+ChatUtils:AddTrans("ruRU", "LID_USESMALLCHANNELNAMES", "Использовать короткие названия каналов")
