33094c7e1d15bc832b395a1effb2048495d969ee
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-05-13 12:08:25 +0200

    v0.3.81

--- settings.lua
+++ settings.lua
@@ -6,7 +6,7 @@ chutSetup:SetScript(
     function(self, event, ...)
         if event == "PLAYER_LOGIN" then
             CHUT = CHUT or {}
-            ChatUtils:SetVersion(133457, "0.3.80")
+            ChatUtils:SetVersion(133457, "0.3.81")
             ChatUtils:AddSlash("chut", ChatUtils.ToggleSettings)
             ChatUtils:AddSlash("chatutils", ChatUtils.ToggleSettings)
             ChatUtils:SetAddonOutput("ChatUtils", 133457)
